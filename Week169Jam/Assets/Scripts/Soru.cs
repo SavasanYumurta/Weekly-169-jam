34f89da548fd5992721ce28dbdf0fe7c573aa6e0
SavasanYumurta <72253010+SavasanYumurta@users.noreply.github.com>
2020-10-03 12:16:43 +0300

    sorusistem2

--- Week169Jam/Assets/Scripts/Soru.cs
+++ Week169Jam/Assets/Scripts/Soru.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName ="Soru")]
 public class Soru : ScriptableObject
 {
-    [TextArea(4,6)][SerializeField] private string soru;
+    [TextArea(4,6)] public string soru;
     [SerializeField] private string birinciC,ikinciC,UcuncuC;
+    [SerializeField] private Soru C1Soru, C2Soru, C3Soru;
+
+    public Soru GetSiradakiSoru()
+    {
+        return C1Soru;
+    }
 }
---
+++ Week169Jam/Assets/Scripts/SoruKontrol.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoruKontrol : MonoBehaviour
+{
+    [SerializeField] private Soru baslangicSorusu;
+    private Soru mevcutSoru;
+    [SerializeField] private GameObject soruBolum;
+    private void Start()
+    {
+        mevcutSoru = baslangicSorusu;
+        SorularıAl();
+    }
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            mevcutSoru = mevcutSoru.GetSiradakiSoru();
+            SorularıAl();
+        }
+    }
+    private void SorularıAl()
+    {
+        soruBolum.transform.GetChild(0).GetComponent<Text>().text = mevcutSoru.soru;
+    }
+}
