35167db847ec31b1ed7c0f54dadeba9bf15d1b9d
Yavuz124 <y.selimk@hotmail.com>
2020-10-03 18:01:55 +0300

    Top
    tt

--- Week169Jam/Assets/Scripts/Top.cs
+++ Week169Jam/Assets/Scripts/Top.cs
@@ -7,19 +7,35 @@ public class Top : MonoBehaviour
     public GameObject mermi;
     public GameObject yazi;
     public GameObject Topy;
-    public void OnTriggerEnter2D(Collider2D collision)
+
+    public void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collision.transform.tag == "TopMermi")
+        if (collider.transform.tag == "TopMermi")
         {
             Debug.Log("Deydi");
             yazi.SetActive(true);
+        }
+    }
+    public void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.transform.tag == "TopMermi")
+        {
+            Debug.Log("Deydi");
+            yazi.SetActive(false);
+        }
+    }
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+       
+    if (collision.transform.tag == "TopMermi")
+           {
             if (Input.GetKeyDown(KeyCode.F))
             {
                 mermi.SetActive(true);
                 StartCoroutine(Topp());
             }
-
-        }
+           }
+        
     }
     IEnumerator Topp()
     {
