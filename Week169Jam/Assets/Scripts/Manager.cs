bf48aa8b3dc6302a240686a9d98854922a4e3ba2
SavasanYumurta <72253010+SavasanYumurta@users.noreply.github.com>
2020-10-03 13:11:16 +0300

    mapAcKapa

--- Week169Jam/Assets/Scripts/Manager.cs
+++ Week169Jam/Assets/Scripts/Manager.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SoruKontrol : MonoBehaviour
+public class Manager : MonoBehaviour
 {
     [SerializeField] private Soru baslangicSorusu;
     private Soru mevcutSoru;
+    private bool map;
     [SerializeField] private GameObject soruBolum;
+    
     private void Start()
     {
         mevcutSoru = baslangicSorusu;
@@ -15,19 +17,22 @@ public class SoruKontrol : MonoBehaviour
     }
     public void Update()
     {
-        SorularıAl();
+        if (map)
+        {
+            SorularıAl();
 
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            mevcutSoru = mevcutSoru.GetSiradakiSoru1();
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            mevcutSoru = mevcutSoru.GetSiradakiSoru2();
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            mevcutSoru = mevcutSoru.GetSiradakiSoru3();
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                mevcutSoru = mevcutSoru.GetSiradakiSoru1();
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                mevcutSoru = mevcutSoru.GetSiradakiSoru2();
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                mevcutSoru = mevcutSoru.GetSiradakiSoru3();
+            }
         }
     }
     private void SorularıAl()
@@ -36,5 +41,10 @@ public class SoruKontrol : MonoBehaviour
         soruBolum.transform.GetChild(1).GetComponent<Text>().text = mevcutSoru.birinciC;
         soruBolum.transform.GetChild(2).GetComponent<Text>().text = mevcutSoru.ikinciC;
         soruBolum.transform.GetChild(3).GetComponent<Text>().text = mevcutSoru.UcuncuC;
+    }
+    public void MapAc()
+    {
+        map = !map;
+        transform.GetChild(0).gameObject.SetActive(map);
     }
 }
