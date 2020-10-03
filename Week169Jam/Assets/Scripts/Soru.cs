e3209710d44997ba57d54885556a576800759428
SavasanYumurta <72253010+SavasanYumurta@users.noreply.github.com>
2020-10-03 12:00:56 +0300

    SoruSistemi

---
+++ Week169Jam/Assets/Scripts/Soru.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Soru")]
+public class Soru : ScriptableObject
+{
+    [TextArea(4,6)][SerializeField] private string soru;
+    [SerializeField] private string birinciC,ikinciC,UcuncuC;
+}
