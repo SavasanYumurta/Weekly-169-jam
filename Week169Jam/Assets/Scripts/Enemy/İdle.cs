f4d2f820124a6e14a52d87c47762c52f634b06d8
SavasanYumurta <72253010+SavasanYumurta@users.noreply.github.com>
2020-10-03 17:17:25 +0300

    playerprefab

--- Week169Jam/Assets/Scripts/Enemy/İdle.cs
+++ Week169Jam/Assets/Scripts/Enemy/İdle.cs
@@ -2,23 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FireGo : StateMachineBehaviour
+public class İdle : StateMachineBehaviour
 {
-    [SerializeField]private Transform topos;
-    [SerializeField]private float speed,offsetx,offsety;
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-      topos = GameObject.FindWithTag("EnemyTop").transform;
+        animator.SetBool("isGoFire", true);
     }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.transform.position = Vector2.MoveTowards(animator.transform.position,new Vector3(topos.position.x + offsetx,topos.position.y + offsety),speed * Time.deltaTime);
-        if (Vector2.Distance(animator.transform.position, new Vector3(topos.position.x + offsetx, topos.position.y + offsety)) <= 0.1)
-        {
-            animator.SetBool("isFire", true);
-            animator.SetBool("isGoFire", false);
-        }
+        
     }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         
