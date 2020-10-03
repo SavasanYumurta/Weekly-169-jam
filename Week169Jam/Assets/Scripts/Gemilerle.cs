9faf942a263252c5104dd6b043d05e6f503d89c3
SavasanYumurta <72253010+SavasanYumurta@users.noreply.github.com>
2020-10-03 14:03:04 +0300

    karakter

---
+++ Week169Jam/Assets/Scripts/Gemilerle.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gemilerle : MonoBehaviour
+{
+ 
+    public void Update()
+    {
+        this.GetComponent<Rigidbody2D>().velocity = new Vector2(0.3f, 0);
+    }
+}
